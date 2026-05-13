5bd1630eea371fda685ef9cd1ae270249e28557e
charliekerfoot <charliekerfoot@gmail.com>
2026-05-12 23:41:18 -0500

    Completely refactored the website

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-export { default as StartNav } from './StartNav.svelte';
-export { default as Footer } from './Footer.svelte';
-export { default as ThemeToggle } from './ThemeToggle.svelte';
-export { default as Epigraph } from './Epigraph.svelte';
-export { default as Footnote } from './Footnote.svelte';
